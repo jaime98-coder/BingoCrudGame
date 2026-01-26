73b694442fc02deff7663200cc5997e95fc6d1a0
GonzaloPG <gonzalo.prada@iesnervion.es>
2026-01-26 10:03:01 +0100

    260126

--- src/bingocrud/Player.java
+++ src/bingocrud/Player.java
@@ -13,16 +13,54 @@ showCard() → Muestra el cartón por consola o interfaz.*/
 package bingocrud;
 
 public class Player {
-	private String name;          // Nombre del jugador
-	private BingoCard card;       // Su cartón de Bingo
-	private boolean hasRow;       // Para saber si ya cantó ROW
-	private boolean hasBingo;     // Para saber si ya cantó BINGO
-	
-	//Constructor de la clase 
-	public Player(String name, Bingocard card) {
+	private String name; // Nombre del jugador
+	private BingoCard card; // Su cartón de Bingo
+	private boolean hasRow; // Para saber si ya cantó ROW
+	private boolean hasBingo; // Para saber si ya cantó BINGO
+
+	// Constructor de la clase
+	public Player(String name, BingoCard card) {
 		this.name = name;
 		this.card = card;
 		this.hasRow = false;
 		this.hasBingo = false;
 	}
-}
+
+	// Creo que el getName del usuario para controlarlo
+	public String getName() {
+
+		return this.name;
+	}
+
+	// Creo el getCard que devuelve el Card
+	public int[][] getCard
+	{
+
+		return card;
+	}
+
+	// Creo el set name para darle valor al name
+	public void setName(String name) {
+
+		// el name no puede estar vacio
+		if (name.isEmpty()) {
+
+			System.out.println("El nombre no puede estar vacio");
+
+		} else {
+
+
+		}
+	}
+
+	public void markCard(int numeroX, int numeroY) {
+
+		int marcado = -1;
+
+		int[][] cart = card.getCard();
+
+		cart[numeroX][numeroY] = marcado;
+
+	}
+
+ }
