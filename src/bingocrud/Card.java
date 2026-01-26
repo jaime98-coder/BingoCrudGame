a44fe2cd5d9ea3e200485b98382b3dae0ced2ec0
jaime98-coder <jaime.martin@iesnervion.es>
2026-01-26 09:40:49 +0100

    Subida de metodos y atributo clase card

--- src/bingocrud/Card.java
+++ src/bingocrud/Card.java
@@ -1,5 +1,31 @@
 package bingocrud;
 
-public class Card {
+import java.util.Random;
 
+public class Card {
+	int[][] card = new int[3][5];
+	Random rd = new Random();
+
+	private int generateNumber() {
+		int cardNumber;
+
+		cardNumber = rd.nextInt(1, 100);
+		return cardNumber;
+	}
+
+	public void fillCard() {
+		for (int i = 0; i < card.length; i++) {
+			for (int j = 0; j < card[i].length; j++) {
+
+			}
+		}
+	}
+
+	public void showCard() {
+		for (int i = 0; i < card.length; i++) {
+			for (int j = 0; j < card[i].length; j++) {
+
+			}
+		}
+	}
 }
