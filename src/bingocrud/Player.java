488cd3c1b82b92fc0221442bc695fec897f3a8f8
nicms06 <nicolas.mingorance@iesnervion.es.com>
2026-01-26 09:24:35 +0100

    Player

--- src/bingocrud/Player.java
+++ src/bingocrud/Player.java
@@ -1,5 +1,22 @@
+/*getName() → Devuelve el nombre del jugador.
+
+getCard() → Devuelve su cartón.
+
+markNumber(number) → Marca un número en el cartón.
+
+checkRow() → Comprueba si tiene una fila completa y actualiza hasRow.
+
+checkBingo() → Comprueba si tiene BINGO completo y actualiza hasBingo.
+
+showCard() → Muestra el cartón por consola o interfaz.*/
+
 package bingocrud;
 
 public class Player {
-
+	private String name;          // Nombre del jugador
+	private BingoCard card;       // Su cartón de Bingo
+	private boolean hasRow;       // Para saber si ya cantó ROW
+	private boolean hasBingo;     // Para saber si ya cantó BINGO
+	
+	
 }
