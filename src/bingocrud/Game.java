f69bb53d5db91553f8c4bffd428c63dd2fd64cae
GonzaloPG <gonzalo.prada@iesnervion.es>
2026-03-06 12:05:35 +0100

    Rename player variables for consistency

--- src/bingocrud/Game.java
+++ src/bingocrud/Game.java
@@ -9,13 +9,13 @@ public class Game {
         Caller caller = new Caller();
         
         // Initialize players
-        Player p1 = new Player("Player 1");
-        Player p2 = new Player("Player 2");
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
 
         // Show initial cards
-        p1.showCard();
+        player1.showCard();
         System.out.println(); 
-        p2.showCard();
+        player2.showCard();
 
         boolean gameOver = false;
         
@@ -39,21 +39,21 @@ public class Game {
             System.out.println("Number drawn: " + currentNumber);
 
             // --- Player 1 Turn ---
-            p1.markNumber(currentNumber);
-            p1.checkRow(); // Check for line
-            if (p1.checkBingo()) {
+            player1.markNumber(currentNumber);
+            player1.checkRow(); // Check for line
+            if (player1.checkBingo()) {
                 System.out.println("-----------------------------------------------");
-                System.out.println("WINNER: " + p1.getName());
+                System.out.println("WINNER: " + player1.getName());
                 gameOver = true;
             }
 
             // --- Player 2 Turn (only if game is not over) ---
             if (!gameOver) {
-                p2.markNumber(currentNumber);
-                p2.checkRow(); // Check for line
-                if (p2.checkBingo()) {
+                player2.markNumber(currentNumber);
+                player2.checkRow(); // Check for line
+                if (player2.checkBingo()) {
                     System.out.println("-----------------------------------------------");
-                    System.out.println("WINNER: " + p2.getName());
+                    System.out.println("WINNER: " + player2.getName());
                     gameOver = true;
                 }
             }
